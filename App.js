f8ae17009d4bd9bff265565c1c05fb6aa9083826
Veronica Adler <vlynnadler@gmail.com>
2021-08-29 18:47:09 -0400

    updated styling and added input form

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import Header from "./components/header";
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import TodoItem from "./components/TodoItem";
+import { StyleSheet, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import AddTodo from "./components/addTodo"
 
 
 export default function App() {
@@ -8,22 +10,50 @@ export default function App() {
     { text: 'mop floor', key: "1"},
     { text: 'oil change', key: "2"},
     { text: 'call mom', key: "3"}
+  ]);
 
-  ])
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key);
+    });
+  }
+
+  const submitHandler = (text) => {
+
+    if(text.length > 3){
+      setTodos((prevTodos) => {
+        return [
+          { text: text, key: Math.random().toString() },
+          ...prevTodos
+        ];
+      })
+    } else {
+      Alert.alert('OOPS!', "todos must be over 3 chars long", [{
+        text: 'Understood', onPress: () => console.log('alert closed')
+      }])
+    }
+
+  }
+
   return (
-    <View style={styles.container}>
-        <Header />
-      <View style={styles.content}>
-        <View style={styles.list}>
-            <FlatList 
-              data={todos}
-              renderItem={({item}) => (
-                <Text>{item.text}</Text>
-              )}
-            />
+    <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss();
+    }}>
+      <View style={styles.container}>
+          <Header />
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler} />
+          <View style={styles.list}>
+              <FlatList 
+                data={todos}
+                renderItem={({item}) => (
+                  <TodoItem item={item} pressHandler={pressHandler}/>
+                )}
+              />
+          </View>
         </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -34,8 +64,10 @@ const styles = StyleSheet.create({
   },
   content: {
     padding: 40,
+    flex: 1
   },
   list: {
     marginTop: 20,
+    flex: 1
   },
 });
